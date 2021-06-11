91560be7ac7cb90281a6fcf97067c235d3057dd8
Gyouza71 <85442605+Gyouza71@users.noreply.github.com>
2021-06-11 10:55:02 +0900

    Update app.py 11-2

--- web/app.py
+++ web/app.py
@@ -10,7 +10,8 @@ def mainmenu():
     <html>
     <body>
     <center><h1>Hello World! from AWS CodeDeploy.</h1><br/>
-    <h2>Piper CI/CD Lab11-1<h2>
+    <h2>Piper CI/CD Lab11-2</h2>
+    <h2>2021/6/11</h2>
     </body>
     </html>"""
 
